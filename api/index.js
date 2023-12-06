143a62edd1d3bf38c3746f72716eddef4bd8c410
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-12-06 16:33:42 +0530

    Update and rename vk to index.js

---
+++ api/index.js
@@ -0,0 +1,41 @@
+// Assuming you're using Express for your server
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = 3000;
+
+const rapidApiKey = '564ea2c962msh3a29125df3c8c9ep14d798jsnb011c7b66f1b';
+
+app.get('/audio', async (req, res) => {
+  try {
+    const youtubeUrl = req.query.url;
+
+    if (!youtubeUrl) {
+      return res.status(400).json({ error: 'Please provide a valid YouTube URL' });
+    }
+
+    const response = await axios.request({
+      method: 'GET',
+      url: 'https://youtube-mp36.p.rapidapi.com/dl',
+      params: { id: youtubeUrl },
+      headers: {
+        'X-RapidAPI-Key': rapidApiKey,
+        'X-RapidAPI-Host': 'youtube-mp36.p.rapidapi.com',
+      },
+    });
+
+    if (response.data.status === 'ok') {
+      // Handle successful response here, maybe send the audio file to the client
+      res.json({ message: 'Audio downloaded successfully' });
+    } else {
+      res.status(500).json({ error: response.data.msg || 'Unknown error occurred' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
